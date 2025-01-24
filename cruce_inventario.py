3c1613441c6cebb01823ff2032490d1032ebd02b
Jose Tude <jose.tudelano@cgmrental.com.pe>
2025-01-24 17:09:30 -0500

    segundo cambio

--- cruce_inventario.py
+++ cruce_inventario.py
@@ -16,20 +16,17 @@ def cruce_inventarios(connection):
         consult_data(connection,f"truncate table {tbl_cruce_inventario}")
 
         inventario_mb52 = consult_data(connection,f"select * from {tbl_inventario_mb52}")
-        inventario_ewm = consult_data(connection,f"select * from {tbl_inventario_ewm}")
+        inventario_ewm = consult_data(connection,f"select Producto,Propietario,Lote,Tipo_stock,ctd_producto from {tbl_inventario_ewm}")
 
         inventario_mb = pd.DataFrame(inventario_mb52)
         inventario_sc = pd.DataFrame(inventario_ewm)
-
-        inventario_sc_filtrado = inventario_sc[['Producto','Propietario','Lote','Tipo_stock','ctd_producto']]
-        inventario_sc_fil = inventario_sc_filtrado.copy()
 
         filtrar_inventario_mb = inventario_mb.loc[inventario_mb['alm'].isin(['0002','0012'])].reset_index()
         agrupar_inventario_mb = filtrar_inventario_mb.groupby(['material','materialtxt','centro','tipomaterial','grupoarticulo'])['cantidad'].sum().reset_index()
         agrupar_inventario_mb['union'] = agrupar_inventario_mb['material'] +''+agrupar_inventario_mb['centro']
 
 
-        filtrar_inventario_sc = inventario_sc_fil.loc[inventario_sc_fil['Tipo_stock'].isin(['S2','M2'])].reset_index()
+        filtrar_inventario_sc = inventario_sc.loc[inventario_sc['Tipo_stock'].isin(['S2','M2'])].reset_index()
         pivotear_inventario_sc = filtrar_inventario_sc.pivot_table(index=['Producto','Propietario'],columns='Lote',values='ctd_producto', aggfunc='sum',fill_value=0)
         inv_sc = pivotear_inventario_sc.reset_index()
         inv_sc.rename(columns={inv_sc.columns[3]:'Sin_Asignacion'},inplace=True)
